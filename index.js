c134280be65632d8f4ad588a4d307672b8c91b1c
Ayelen Juarez <ayelenjuarez@hotmail.com>
2022-04-29 17:00:54 -0300

    sumando arrays con objetos

--- index.js
+++ index.js
@@ -7,11 +7,33 @@ let nombre = prompt("Ingresa tu Nombre");
         alert("Ingresaste un nombre invalido");
     }
 
+function capturar(){
+    function Persona (nombre,edad){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    const nombreCapturar = document.getElementById("nombre").value;
+    console.log(nombreCapturar);
+    
+    const edadCapturar = document.getElementById("edad").value;
+    console.log(edadCapturar);
+
+    nuevoSujeto = new Persona (nombreCapturar, edadCapturar)
+    console.log(nuevoSujeto);
+    agregar();
+}
+
+var baseDatos= [];
+function agregar(){
+    baseDatos.push(nuevoSujeto);
+    console.log(baseDatos);
+    document.getElementById("tabla").innerHTML += '<tbody><td>' + nuevoSujeto.nombre + '</td><td>' + nuevoSujeto.edad + '</td></tbody>';
+}
 
 function generarTabla(numero) {
     let num = parseInt(numero);
 
-    let tabla = document.getElementById("tabla");
+    let tabla = document.getElementById("tablas");
     tabla.innerHTML = '';
 
     for (let contador = 1; contador <= 10; contador++) {
